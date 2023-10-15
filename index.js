047fe8c9329d3a72b4a84079739463813444c36e
Devansh Rajput <rajputdevansh012@gmail.com>
2023-10-15 11:37:17 +0530

    Update my file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fs = require("fs");
-const properties = require("./MOCK_DATA.json")
+const estates = require("./MOCK_DATA.json")
 
 const app = express();
 const PORT = 8000;
@@ -8,43 +8,48 @@ const PORT = 8000;
 //Middleware
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/api/properties', (req, res) => {
-  return res.json(properties);
+app.get('/api/estates', (req, res) => {
+  return res.json(estates);
 });
 
-app.get('/api/properties/:id', (req, res) => {
+app.get('/api/estates/:id', (req, res) => {
   const id = Number(req.params.id);
-  const property = properties.find((property) => property.id === id);
-  return res.json(property);
+  const estate = estates.find((estate) => estate.id === id);
+  return res.json(estate);
 });
 
-app.get('/api/properties/type/:type', (req, res) => {
+app.get('/api/estates/type/:type', (req, res) => {
   const type = req.params.type;
-  const property = properties.filter((property) => property.Property_type === type);
-  return res.json(property);
+  const estate = estates.filter((estate) => estate.Property_type === type);
+  return res.json(estate);
 });
 
-app.get('/api/properties/bedrooms/:count', (req, res) => {
+app.get('/api/estates/bedrooms/:count', (req, res) => {
   const bedrooms = Number(req.params.count);
-  const filteredProperties = properties.filter((property) => property.Bedrooms >= bedrooms);
-  return res.json(filteredProperties);
+  const filteredestates = estates.filter((estate) => estate.Bedrooms >= bedrooms);
+  return res.json(filteredestates);
 });
 
 
-app.post('/api/properties', (req, res) => {
+app.post('/api/estates', (req, res) => {
   const body = req.body;
-  properties.push({ ...body, id: properties.length + 1 });
-  fs.writeFile('./MOCK_DATA.json', JSON.stringify(properties), (err, data) => {
-    return res.json({ status: "success", id: properties.length + 1 });
+  estates.push({ ...body, id: estates.length + 1 });
+  fs.writeFile('./MOCK_DATA.json', JSON.stringify(estates), (err, data) => {
+    return res.json({ status: "success", id: estates.length});
   });
 });
 
-app.patch('/api/properties/:id', (req, res) => {
+app.put('/api/estates', (req, res) => {
   //Todo
   return res.json({ status: "pending" });
 });
 
-app.delete('/api/properties/:id', (req, res) => {
+app.patch('/api/estates/:id', (req, res) => {
+  //Todo
+  return res.json({ status: "pending" });
+});
+
+app.delete('/api/estates/:id', (req, res) => {
   //Todo
   return res.json({ status: "pending" });
 });
